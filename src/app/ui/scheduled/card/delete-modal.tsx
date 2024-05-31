4ddc6f11faa2d97dbfe819d06f110548d49c8d14
dansaa8 <dansaa8@protonmail.com>
2024-05-31 14:27:13 +0200

    Add basic and ok structure/display or delete-modal.

--- src/app/ui/scheduled/card/delete-modal.tsx
+++ src/app/ui/scheduled/card/delete-modal.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import CancelIcon from '@/app/ui/svg/cancel-icon';
 import FormButton from '@/app/ui/common/FormButton';
+import { Button } from "@nextui-org/react";
 import deleteTodoAction from '@/app/actions/delete-todo';
 import { useContext } from 'react';
 import { TodoContext } from '@/app/providers';
@@ -24,8 +25,9 @@ export default function DeleteModal({ handleModalClose }) {
 
     document.addEventListener('mousedown', handleClickOutside);
 
-    return () => { // cleanup func. Will be called when Modalcomponent is removed.
-      document.body.classList.remove('overflow-hidden'); 
+    return () => {
+      // cleanup func. Will be called when Modalcomponent is removed.
+      document.body.classList.remove('overflow-hidden');
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [handleModalClose]);
@@ -38,12 +40,22 @@ export default function DeleteModal({ handleModalClose }) {
       >
         <div className="fixed inset-0"></div>
       </div>
-      <div className="z-20 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-10 bg-white h-80 w-96 rounded-xl" ref={modalContentRef}>
+      <div
+        className="z-20 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-10 bg-white h-80 w-96 rounded-xl flex flex-col justify-between"
+        ref={modalContentRef}
+      >
         <CancelIcon
           className="absolute top-2 right-2  cursor-pointer  hover:bg-gray-200 rounded-xl"
           onClick={handleModalClose}
         />
-        <div className="flex flex-col justify-between h-full items-center">
+        <h3 className="font-bold text-stone-700 col-start-2 col-end-4 text-center">
+          {todo!.name}
+        </h3>
+
+        <p className='text-sm'>
+          Are you sure you want to remove this task?
+        </p>
+        <div className="flex justify-around">
           <input type="hidden" name="id" value={todo.id} />
           <FormButton
             className="rounded-xl bg-red-600 px-4 py-2 text-white z-0 hover:bg-red-700"
@@ -52,6 +64,12 @@ export default function DeleteModal({ handleModalClose }) {
           >
             Delete
           </FormButton>
+          <Button
+            className="rounded-xl bg-gray-200 px-4 py-2 text-gray-800 z-0 hover:bg-gray-300"
+            onClick={handleModalClose}
+          >
+            Cancel
+          </Button>
         </div>
       </div>
     </form>,
