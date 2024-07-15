8ff0059c94bdfb0ae676d27931f22562e8e7e48d
dansaa8 <dansaa8@protonmail.com>
2024-07-15 18:48:00 +0200

    Remove header from the navbar for easier usage on mobile phones

--- src/components/navigation/sidenav.tsx
+++ src/components/navigation/sidenav.tsx
@@ -4,7 +4,7 @@ import NavLinks from '@/components/navigation/nav-links';
 export default function SideNav() {
   return (
     <header className="bg-emerald-400 text-white text-xl font-bold shadow-md pb-0.5">
-      <h1 className="p-4">Todo-manager</h1>
+      {/* <h1 className="p-4">Todo-manager</h1> */}
       <nav className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
       </nav>
